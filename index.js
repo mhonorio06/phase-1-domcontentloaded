758cf47a0cd2ffaad9c2d3cc2dfcfd3724daaef1
Manny <mhfernan35277@gmail.com>
2023-08-02 19:56:26 -0700

    complete domContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    updateDom()    
+});
+function updateDom () {
+    let p = document.getElementById('text')
+    p.textContent = "This is really cool!";
+};
+
